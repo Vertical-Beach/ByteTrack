39b1034876b15d4f70d360df503120677ed1a512
lp6m <lp6m2014@gmail.com>
2022-01-02 17:33:57 +0000

    add yolov4-tiny Predictor, support in test_predictor.py

--- tools/predictor.py
+++ tools/predictor.py
@@ -1,8 +1,11 @@
-import torch
 import os.path as osp
-
+import os
+from loguru import logger
 from yolox.data.data_augment import preproc
-from yolox.utils import postprocess
+from yolox.utils import fuse_model, get_model_info, postprocess
+
+import torch
+import cv2
 
 class Predictor(object):
     def __init__(
@@ -48,7 +51,7 @@ class Predictor(object):
         img_info["raw_img"] = img
 
         img, ratio = preproc(img, self.test_size, self.rgb_means, self.std)
-        img_info["ratio"] = ratio
+        img_info["ratio"] = (ratio, ratio)
         img = torch.from_numpy(img).unsqueeze(0).float().to(self.device)
         if self.fp16:
             img = img.half()  # to FP16
@@ -63,3 +66,115 @@ class Predictor(object):
             )
             #logger.info("Infer time: {:.4f}s".format(time.time() - t0))
         return outputs, img_info
+
+import darknet.darknet as darknet
+
+class YOLOv4Predictor(Predictor):
+    def __init__(
+        self,
+        network,
+        class_names,
+        class_colors,
+        exp
+    ):
+        self.confthre = exp.test_conf
+        self.nmsthre = exp.nmsthre
+        self.network = network
+        self.class_names = class_names
+        self.class_colors = class_colors
+        self.class_name_dic = {x: i for i, x in enumerate(class_names)}
+
+
+    def inference(self, img, timer):
+        net_width = darknet.network_width(self.network)
+        net_height = darknet.network_height(self.network)
+
+        # img_info
+        img_info = {"id": 0}
+        img_info["file_name"] = None
+        height, width = img.shape[:2]
+        img_info["height"] = height
+        img_info["width"] = width
+        img_info["raw_img"] = img
+        img_info["ratio"] = (net_width/width, net_height/height)
+        # preprocessing
+        darknet_image = darknet.make_image(net_width, net_height, 3)
+
+        image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image_resized = cv2.resize(image_rgb, (net_width, net_height),
+                                interpolation=cv2.INTER_LINEAR)
+        darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
+        # inference
+        timer.tic()
+        detections = darknet.detect_image(self.network, self.class_names, darknet_image, thresh=self.confthre, nms=self.nmsthre)
+        darknet.free_image(darknet_image)
+
+        outputs = []
+        for label, confidence, bbox in detections:
+            class_id = self.class_name_dic[label]
+            left, top, right, bottom = darknet.bbox2points(bbox)
+            confidence = float(confidence)/100.0
+            outputs.append([left, top, right, bottom, confidence, class_id])
+        # wrap as batched list
+        outputs = [outputs]
+        return outputs, img_info
+
+
+def get_yolox_predictor(exp, args, output_dir):
+    if isinstance(args.device, str):
+        args.device = torch.device("cuda" if args.device == "gpu" else "cpu")
+
+    model = exp.get_model().to(args.device)
+    logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
+    model.eval()
+
+    if not args.trt:
+        if args.ckpt is None:
+            ckpt_file = osp.join(output_dir, "best_ckpt.pth.tar")
+        else:
+            ckpt_file = args.ckpt
+        logger.info("loading checkpoint")
+        ckpt = torch.load(ckpt_file, map_location="cpu")
+        # load the model state dict
+        model.load_state_dict(ckpt["model"])
+        logger.info("loaded checkpoint done.")
+
+    if args.fuse:
+        logger.info("\tFusing model...")
+        model = fuse_model(model)
+
+    if args.fp16:
+        model = model.half()  # to FP16
+
+    if args.trt:
+        assert not args.fuse, "TensorRT model is not support model fusing!"
+        trt_file = osp.join(output_dir, "model_trt.pth")
+        assert osp.exists(
+            trt_file
+        ), "TensorRT model is not found!\n Run python3 tools/trt.py first!"
+        model.head.decode_in_inference = False
+        decoder = model.head.decode_outputs
+        logger.info("Using TensorRT to inference")
+    else:
+        trt_file = None
+        decoder = None
+
+    return Predictor(model, exp, trt_file=trt_file, decoder=decoder, device=args.device, fp16=args.fp16)
+
+def get_darknet_predictor(exp, args):
+    network, class_names, class_colors = exp.get_model(
+        args.config_file,
+        args.data_file,
+        args.weights_file
+    )
+    return YOLOv4Predictor(network, class_names, class_colors, exp)
+
+def get_predictor(exp, args) -> Predictor:
+    output_dir = osp.join(exp.output_dir, args.experiment_name)
+    os.makedirs(output_dir, exist_ok=True)
+    if args.model_type == 'yolox':
+        return get_yolox_predictor(exp, args, output_dir)
+    elif args.model_type == 'darknet':
+        return get_darknet_predictor(exp, args)
+    else:
+        raise Exception("unreachable")
--- tools/test_predictor.py
+++ tools/test_predictor.py
@@ -1,22 +1,24 @@
-from tools.demo_track import Predictor
-from torch._C import device
+from tools.predictor import get_predictor
 from yolox.exp import get_exp
-from yolox.utils import fuse_model, get_model_info, postprocess
 from yolox.tracking_utils.timer import Timer
 import cv2
-import argparse
 import torch
+import argparse
+import numpy as np
 
 def make_parser():
     parser = argparse.ArgumentParser("Test Predictor!")
     # parser.add_argument(
     #     "demo", default="image", help="demo type, eg. image, video and webcam"
     # )
+    parser.add_argument("-expn", "--experiment-name", type=str, default=None)
+
     parser.add_argument("-n", "--name", type=str, default=None, help="model name")
 
     # parser.add_argument(
         # "--path", default="./videos/palace.mp4", help="path to images or video"
     # )
+    parser.add_argument("--model_type", type=str, default="yolox", choices=["yolox", "darknet"])
     parser.add_argument(
         "--img_path", default="./images/image_0090.jpg"
     )
@@ -35,6 +37,13 @@ def make_parser():
         default=None,
         type=str,
         help="pls input your expriment description file",
+    )
+    parser.add_argument(
+        "--trt",
+        dest="trt",
+        default=False,
+        action="store_true",
+        help="Using TensorRT model for testing.",
     )
     parser.add_argument(
         "--fp16",
@@ -57,42 +66,62 @@ def make_parser():
         help="threshold for drawing detection box"
     )
     parser.add_argument("-c", "--ckpt", default=None, type=str, help="ckpt for eval")
+    parser.add_argument(
+        "--config_file",
+        help="darknet config file(.cfg)",
+        default=None
+    )
+    parser.add_argument(
+        "--data_file",
+        help="darknet data file(.data)",
+        default=None
+    )
+    parser.add_argument(
+        "--weights_file",
+        help="darknet weights file(.weights)",
+        default=None
+    )
+
     return parser
 
-def draw_yolox_outputs(img, outputs, img_info):
+def draw_bboxes(img, outputs, img_info):
     #assume batch size is 1
     assert(len(outputs) == 1)
-    outputs = outputs[0].cpu().numpy()
+    outputs = outputs[0]
+    if isinstance(outputs, torch.Tensor):
+        # yoloX
+        # (x1, y1, x2, y2, obj_conf, class_conf, class_pred)
+        outputs = outputs.cpu().numpy()
+        scores = outputs[:, 4] * outputs[:, 5]
+        bboxes = outputs[:, :4]
+    else:
+        outputs = np.array(outputs)
+        # darknet
+        # (x1, y1, x2, y2, bbox_conf, class_pred)
+        scores = outputs[:, 4]
+        bboxes = outputs[:, :4]
+
+    print(outputs.shape)
     ratio = img_info["ratio"]
-    for x1, y1, x2, y2, obj_conf, class_conf, class_pred in outputs:
-        x1, y1, x2, y2 = [x/ratio for x in [x1, y1, x2, y2]]
-        x1, y1, x2, y2, class_pred = [int(x) for x in [x1, y1, x2, y2, class_pred]]
-        if class_conf * obj_conf > args.conf_thresh:
-            print(x1, y1, x2, y2, class_conf, class_pred)
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    for ((x1, y1, x2, y2), score) in zip(bboxes, scores):
+        x1, x2 = [x/ratio[0] for x in [x1, x2]]
+        y1, y2 = [y/ratio[1] for y in [y1, y2]]
+        x1, y1, x2, y2 = [int(x) for x in [x1, y1, x2, y2]]
+        if score > args.conf_thresh:
+            print(x1, y1, x2, y2, score)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
     return img
 
+
 def main(exp, args):
-    device = torch.device("cuda" if args.device == "gpu" else "cpu")
+    if not args.experiment_name:
+        args.experiment_name = exp.exp_name
 
-    model = exp.get_model().to(device)
-    model.eval()
-
-    ckpt_file = args.ckpt
-    ckpt = torch.load(ckpt_file, map_location="cpu")
-    # load the model state dict
-    model.load_state_dict(ckpt["model"])
-
-    if args.fuse:
-        model = fuse_model(model)
-
-    if args.fp16:
-        model = model.half()  # to FP16
-    predictor = Predictor(model, exp, trt_file=None, decoder=None, device=device, fp16=args.fp16)
     timer = Timer()
     img = cv2.imread(args.img_path)
+    predictor = get_predictor(exp, args)
     outputs, img_info = predictor.inference(img, timer)
-    img = draw_yolox_outputs(img, outputs, img_info)
+    img = draw_bboxes(img, outputs, img_info)
     cv2.imwrite(args.output_path, img)
 
 if __name__ == "__main__":
